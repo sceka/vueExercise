708f0277613988c9691a4bf43b6c3395063e7786
sceka <scekicm02@gmail.com>
2021-11-25 13:13:25 +0100

    Use dynamic classes and computed properties

--- app.js
+++ app.js
@@ -7,17 +7,20 @@ const app = Vue.createApp({
 				{
 					title: 'Rich Dad poor Dad',
 					author: 'Robert Kiyosaki',
-					img: '/assets/1.jpeg'
+					img: '/assets/1.jpeg',
+					isFav: true
 				},
 				{
 					title: 'Think and Grow Rick',
 					author: 'Napoleon Hill',
-					img: '/assets/2.jpeg'
+					img: '/assets/2.jpeg',
+					isFav: false
 				},
 				{
 					title: 'Richest Man in Babylon',
 					author: 'George S. Clason',
-					img: '/assets/3.jpeg'
+					img: '/assets/3.jpeg',
+					isFav: true
 				}
 			]
 		};
@@ -25,6 +28,14 @@ const app = Vue.createApp({
 	methods: {
 		toogleShowBook() {
 			this.showBook = !this.showBook;
+		},
+		toogleFavBook(book) {
+			book.isFav = !book.isFav;
+		}
+	},
+	computed: {
+		filteredBooks() {
+			return this.books.filter(book => book.isFav);
 		}
 	}
 });
