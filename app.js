edd618ae676426d9c09962128a7a43e495029080
sceka <scekicm02@gmail.com>
2021-11-25 12:11:24 +0100

    Add more click events

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = Vue.createApp({
 			showBook: 'true',
 			title: 'Rich Dad Poor Dad',
 			author: 'Robert Kiyosaki',
-			age: '74'
+			age: '74',
+			x: 0,
+			y: 0
 		};
 	},
 	methods: {
 		toogleShowBook() {
 			this.showBook = !this.showBook;
+		},
+		handleEvent(e, data) {
+			console.log(e, e.type);
+			if (data) {
+				console.log(data);
+			}
+		},
+		handleMouseMove(e) {
+			this.x = e.offsetX;
+			this.y = e.offsetY;
 		}
 	}
 });
